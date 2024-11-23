5f153f8cc231141c7bd125e7a767825b97b1417a
Mandiegr <nannda076@gmail.com>
2024-11-22 23:12:34 -0300

    Header separation: logic and styling in different files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  color: blue;
-`;
-
-const Logo = styled.div`
-  display: flex;
-  align-items: center;
-
-  img {
-    height: 40px; 
-    width: auto;
-  }
-`;
-
-const MenuButton = styled.button`
-  background: none;
-  border: none;
-  cursor: pointer;
-  color: blue;
-
-  span {
-    display: block;
-    width: 25px;
-    height: 3px;
-    background-color: #004e98;
-    margin: 5px 0;
-    transition: transform 0.2s;
-  }
-
-  &:hover span:nth-child(1) {
-    transform: translateX(5px);
-  }
-
-  &:hover span:nth-child(2) {
-    transform: translateX(-5px);
-  }
-`;
+import { HeaderContainer, Logo, MenuButton } from '../styles/headerStyles';
 
 const Header = () => {
   return (
